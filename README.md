2c425ed052663288d03384ecdf3b5e8ed59f7998
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-11-17 14:53:52 +0100

    saves app types

--- README.md
+++ README.md
@@ -6,6 +6,26 @@
 
 GUI Application that let's the user upload packages to the AUR
 
+## Package Types
+
+There are different options how the package can be built. Depending on your selection, different sections have to be filled out or are automatically generated.
+
+### Binary
+
+If the project already provides a binary file, this is the easiest option. You just have to provide the binary file and the programm will generate the code for you. The package will be saved under `/usr/bin`.
+
+### Make File
+
+If the project provides a make file, this option will run the make command and you don't have to provide anything else.
+
+### Rust Cargo
+
+If the project is a rust crate, you can select this option to compile the code for you. The package will be saved under `/usr/bin`.
+
+### Custom
+
+If none of the above options match your package, you can provide custom scripts.
+
 ## Dependencies
 
 - git
--- src/main_page/imp.rs
+++ src/main_page/imp.rs
@@ -1,18 +1,18 @@
 use std::cell::RefCell;
 
 use adw::traits::MessageDialogExt;
-use adw::{ApplicationWindow, MessageDialog, ToastOverlay, Toast};
-use gtk::gio::{ActionGroup, Settings, SimpleAction, SimpleActionGroup};
+use adw::{ MessageDialog, ToastOverlay, Toast };
+use gtk::gio::{ Settings, SimpleAction, SimpleActionGroup };
 use gtk::glib::variant::ObjectPath;
-use gtk::glib::{self, clone, Variant, VariantType, VariantTy};
+use gtk::glib::{ self, clone, Variant, VariantTy };
 use gtk::subclass::prelude::*;
-use gtk::{CompositeTemplate, Expander};
-use gtk::{prelude::*, Window};
+use gtk::{ CompositeTemplate, Expander, DropDown };
+use gtk::{ prelude::*, Window };
 use once_cell::unsync::OnceCell;
 
 use crate::entry::Entry;
 use crate::list::List;
-use crate::package_manager::{Repository, RepositoryError};
+use crate::package_manager::{ Repository, RepositoryError };
 use crate::APP_ID;
 
 #[derive(CompositeTemplate, Default)]
@@ -73,6 +73,8 @@ pub struct MainPage {
     #[template_child]
     md5: TemplateChild<List>,
     #[template_child]
+    package_type: TemplateChild<DropDown>,
+    #[template_child]
     toast_overlay: TemplateChild<ToastOverlay>,
     #[template_child]
     maintainer_expander: TemplateChild<Expander>,
@@ -103,46 +105,37 @@ impl MainPage {
 
         self.maintainer_name.set_property(
             "content",
-            data.username.clone().or(Some("".to_string())).unwrap(),
+            data.username.clone().or(Some("".to_string())).unwrap()
         );
         self.maintainer_email.set_property(
             "content",
-            data.email.clone().or(Some("".to_string())).unwrap(),
+            data.email.clone().or(Some("".to_string())).unwrap()
         );
         self.package_name.set_property(
             "content",
-            data.name.clone().or(Some("".to_string())).unwrap(),
+            data.name.clone().or(Some("".to_string())).unwrap()
         );
         self.package_version.set_property(
             "content",
-            data.version.clone().or(Some("".to_string())).unwrap(),
+            data.version.clone().or(Some("".to_string())).unwrap()
         );
         self.release_number.set_property(
             "content",
-            data.rel.clone().or(Some("".to_string())).unwrap(),
+            data.rel.clone().or(Some("".to_string())).unwrap()
         );
-        self.epoch.set_property(
-            "content",
-            data.epoch.clone().or(Some("".to_string())).unwrap(),
-        );
+        self.epoch.set_property("content", data.epoch.clone().or(Some("".to_string())).unwrap());
         self.description.set_property(
             "content",
-            data.desc.clone().or(Some("".to_string())).unwrap(),
+            data.desc.clone().or(Some("".to_string())).unwrap()
         );
         self.architectures.set_property("data", data.arch.clone().to_variant());
-        self.url.set_property(
-            "content",
-            data.url.clone().or(Some("".to_string())).unwrap(),
-        );
+        self.url.set_property("content", data.url.clone().or(Some("".to_string())).unwrap());
         self.license.set_property("data", data.license.clone().to_variant());
         self.groups.set_property("data", data.groups.clone().to_variant());
         self.dependencies.set_property("data", data.depends.clone().to_variant());
-        self.makedependencies
-            .set_property("data", data.makedepends.clone().to_variant());
-        self.checkdependencies
-            .set_property("data", data.checkdepends.clone().to_variant());
-        self.optdependencies
-            .set_property("data", data.optdepends.clone().to_variant());
+        self.makedependencies.set_property("data", data.makedepends.clone().to_variant());
+        self.checkdependencies.set_property("data", data.checkdepends.clone().to_variant());
+        self.optdependencies.set_property("data", data.optdepends.clone().to_variant());
         self.provides.set_property("data", data.provides.clone().to_variant());
         self.conflicts.set_property("data", data.conflicts.clone().to_variant());
         self.replaces.set_property("data", data.replaces.clone().to_variant());
@@ -150,16 +143,16 @@ impl MainPage {
         self.options.set_property("data", data.options.clone().to_variant());
         self.install.set_property(
             "content",
-            data.install.clone().or(Some("".to_string())).unwrap(),
+            data.install.clone().or(Some("".to_string())).unwrap()
         );
         self.changelog.set_property(
             "content",
-            data.changelog.clone().or(Some("".to_string())).unwrap(),
+            data.changelog.clone().or(Some("".to_string())).unwrap()
         );
         self.sources.set_property("data", data.source.clone().to_variant());
         self.pgpkeys.set_property("data", data.pgpkeys.clone().to_variant());
         self.md5.set_property("data", data.md5sums.clone().to_variant());
-
+        self.package_type.set_selected(data.package_type as u32);
     }
 
     // saves the state of the widgets to the repository
@@ -173,29 +166,80 @@ impl MainPage {
         data.rel = Some(self.release_number.property("content"));
         data.epoch = Some(self.epoch.property("content"));
         data.desc = Some(self.description.property("content"));
-        data.arch = self.architectures.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
+        data.arch = self.architectures
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
         data.url = Some(self.url.property("content"));
-        data.license = self.license.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.groups = self.groups.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.depends = self.dependencies.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.makedepends = self.makedependencies.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.checkdepends = self.checkdependencies.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.optdepends = self.optdependencies.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.provides = self.provides.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.conflicts = self.conflicts.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.replaces = self.replaces.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.backup = self.backup.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.options = self.options.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
+        data.license = self.license
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.groups = self.groups
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.depends = self.dependencies
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.makedepends = self.makedependencies
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.checkdepends = self.checkdependencies
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.optdepends = self.optdependencies
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.provides = self.provides
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.conflicts = self.conflicts
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.replaces = self.replaces
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.backup = self.backup
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.options = self.options
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
         data.install = Some(self.install.property("content"));
         data.changelog = Some(self.changelog.property("content"));
-        data.source = self.sources.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.noextract = self.noextract.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.pgpkeys = self.pgpkeys.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
-        data.md5sums = self.md5.property::<Variant>("data").get().expect("Value needs to be of type `Vec<String>`!");
+        data.source = self.sources
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.noextract = self.noextract
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.pgpkeys = self.pgpkeys
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        data.md5sums = self.md5
+            .property::<Variant>("data")
+            .get()
+            .expect("Value needs to be of type `Vec<String>`!");
+        if let Some(package_type) = num::FromPrimitive::from_u32(self.package_type.selected()) {
+            data.package_type = package_type;
+        }
     }
 
     // sets state of all expanders
-    fn set_expanded(&self, expanded: bool){
+    fn set_expanded(&self, expanded: bool) {
         self.maintainer_expander.set_expanded(expanded);
         self.name_expander.set_expanded(expanded);
         self.version_expander.set_expanded(expanded);
@@ -206,7 +250,6 @@ impl MainPage {
         self.sources_expander.set_expanded(expanded);
         self.integrity_expander.set_expanded(expanded);
         self.scripts_expander.set_expanded(expanded);
-
     }
 }
 
@@ -234,12 +277,7 @@ impl ObjectImpl for MainPage {
 
         if let Some(path) = path {
             // TODO: good error handling
-            if let Err(err) = self
-            .repository
-            .try_borrow_mut()
-            .unwrap()
-            .load_path(path.as_str())
-            {
+            if let Err(err) = self.repository.try_borrow_mut().unwrap().load_path(path.as_str()) {
                 settings.set("project-path", &None::<ObjectPath>);
                 println!("{}", err);
             }
@@ -264,18 +302,17 @@ impl ObjectImpl for MainPage {
 
                     main_page.populate_widgets();
                 }
-            }),
+            })
         );
 
-        self.settings
-            .set(settings)
-            .expect("Settings should only be set once!");
+        self.settings.set(settings).expect("Settings should only be set once!");
 
         // register actions
         let repo_actions = SimpleActionGroup::new();
 
         let delete_action = SimpleAction::new("delete", None);
-        delete_action.connect_activate(clone!(@weak self as main_page => move |_action, _param|{
+        delete_action.connect_activate(
+            clone!(@weak self as main_page => move |_action, _param|{
             println!("delete");
             let dialog = MessageDialog::new(None as Option<&Window>, Some("Are you sure?"), Some(&format!("Are you sure you want to delete this directory: {} and all it's children?", main_page.repository.borrow().get_path())));
             dialog.add_response("cancel", "No");
@@ -293,10 +330,12 @@ impl ObjectImpl for MainPage {
             }));
 
             dialog.present();
-        }));
+        })
+        );
 
         let save_action = SimpleAction::new("save", None);
-        save_action.connect_activate(clone!(@weak self as main_window => move |_action, _param|{
+        save_action.connect_activate(
+            clone!(@weak self as main_window => move |_action, _param|{
             main_window.save_widget_sate();
             let toast = match main_window.repository.borrow().save_data(){
                 Ok(_) => Toast::new("Project Saved"),
@@ -306,10 +345,12 @@ impl ObjectImpl for MainPage {
             toast.set_timeout(1);
             
             main_window.toast_overlay.add_toast(&toast);
-        }));
+        })
+        );
 
         let publish_action = SimpleAction::new("publish", None);
-        publish_action.connect_activate(clone!(@weak self as main_window => move |_action, _param|{
+        publish_action.connect_activate(
+            clone!(@weak self as main_window => move |_action, _param|{
             // safe window state
             main_window.save_widget_sate();
             if let Err(_) = main_window.repository.borrow().save_data(){
@@ -327,10 +368,12 @@ impl ObjectImpl for MainPage {
             };
 
             main_window.toast_overlay.add_toast(&toast);
-        }));
+        })
+        );
 
         let clear_action = SimpleAction::new("clear", None);
-        clear_action.connect_activate(clone!(@weak self as main_window => move |_action, _param|{
+        clear_action.connect_activate(
+            clone!(@weak self as main_window => move |_action, _param|{
             let toast: Toast;
             if let Ok(mut repo) = main_window.repository.try_borrow_mut(){
                 repo.clear();
@@ -343,16 +386,19 @@ impl ObjectImpl for MainPage {
             }
 
             main_window.toast_overlay.add_toast(&toast);
-        }));
+        })
+        );
 
         let toggle_expander_action = SimpleAction::new("open_expander", Some(VariantTy::BOOLEAN));
-        toggle_expander_action.connect_activate(clone!(@weak self as main_window => move |_action, param|{
+        toggle_expander_action.connect_activate(
+            clone!(@weak self as main_window => move |_action, param|{
             if let Some(param) = param{
                 if let Some(param) = param.get::<bool>(){
                     main_window.set_expanded(param);
                 }
             }
-        }));
+        })
+        );
 
         repo_actions.add_action(&delete_action);
         repo_actions.add_action(&save_action);
